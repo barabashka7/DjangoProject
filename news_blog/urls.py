efb268ebb2247daf51e69a727c4f8b0d129cb1b8
barabashka7 <vadikpro7@mail.ru>
2018-02-18 14:36:57 +0700

    4

--- news_blog/urls.py
+++ news_blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+path('new/', views.new_article, name='new_article'),
     path('', views.index, name='index'),
     path('<int:article_id>/',views.get_article,name='article'),
+    path('<int:article_id>/edit', views.edit_article, name='edit'),
 ]
--- news_blog/views.py
+++ news_blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound,HttpResponse
 from .models import Article, Author, Comment
+from .forms import ArticleForm, CommentForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 
 # Create your views here.
 
@@ -16,10 +17,44 @@ def get_article(request, article_id):
        comment = Comment.objects.filter(article__pk=article_id)
    except (Article.DoesNotExist, Author.DoesNotExist) as error:
        return HttpResponseNotFound(error)
+   if request.method == "POST":
+       form = CommentForm(request.POST)
+       if form.is_valid():
+            comment = form.save(commit=False)
+            comment.published_date = timezone.now()
+            comment.article = article
+            comment.save()
+            return redirect('article', article_id=article.pk)
+   else:
+        form = CommentForm()
    return render(request, 'news_blog/news_article.html',
                  {'article': article,
                   'author': author,
-                  'comment': comment
-
+                  'comment': comment,
+                  'form': form
                   }
                  )
+def new_article(request):
+   if request.method == "POST":
+       form = ArticleForm(request.POST)
+       if form.is_valid():
+           article = form.save(commit=False)
+           article.published_date = timezone.now()
+           article.save()
+           return redirect('article', article_id=article.pk)
+   else:
+       form = ArticleForm()
+   return render(request, 'news_blog/edit_article.html', {'form': form})
+
+def edit_article(request, article_id):
+   article = get_object_or_404(Article, pk=article_id)
+   if request.method == "POST":
+       form = ArticleForm(request.POST, instance=article)
+       if form.is_valid():
+           article = form.save(commit=False)
+           article.published_date = timezone.now()
+           article.save()
+           return redirect('article', article_id=article.pk)
+   else:
+       form = ArticleForm(instance=article)
+   return render(request, 'news_blog/edit_article.html', {'form': form})
